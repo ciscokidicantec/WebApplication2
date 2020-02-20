bd4d8194a47f6b7102dd39594d41e5b513f729a8
ciscokidicantec <cisco.kidicantec@googlemail.com>
2020-02-20 14:50:11 +0000

    Added reader.read function to iterate through rooms table.

--- WebApplication2/WebForm1.aspx.cs
+++ WebApplication2/WebForm1.aspx.cs
@@ -6,8 +6,10 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
 
-
+using System.Configuration;
+//using System.Data.SqlClient.SqlCommand;
 
 namespace WebApplication2
 {
@@ -20,8 +22,54 @@ namespace WebApplication2
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlCommand inscommand;
+            SqlConnection conn;
+            int myid;
+
+
             string tester = "Test";
             Button1.Text = tester;
+
+            try
+            {
+               string cs = ConfigurationManager.ConnectionStrings["newestateConnectionString"].ConnectionString;
+               conn = new System.Data.SqlClient.SqlConnection();
+               conn.ConnectionString = cs;
+
+                string cmdtext = "SELECT * FROM room;";
+                inscommand = new System.Data.SqlClient.SqlCommand(cmdtext, conn);
+                conn.Open();
+
+                SqlDataReader reader = inscommand.ExecuteReader();
+                while (reader.Read())
+                {
+                    myid = (int)reader["roomid"];
+                }
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Response.Write("<br/><br/><br/>Cannot connect to server. " +
+                                        "<br/>Sql Error Message = " + ex.Message +
+                                        "<br/>Sql Error Number = " + ex.Number + "<br/><br/>");
+                        break;
+                    case 1045:
+                        Response.Write("Invalid username/password, please try again" +
+                                       "<br/>Sql Error Message = " + ex.Message +
+                                       "<br/>Sql Error Number = " + ex.Number + "<br/><br/>");
+                        break;
+                    default:
+                        Response.Write("<br/>Unknown Error = " + ex.Number +
+                                        " Error Message = " + ex.Message);
+                        break;
+                }
+            }
+
+
+
+
         }
     }
 }
